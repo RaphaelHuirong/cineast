1094c9cc581842423ffb3b6d9f88b52a95da929c
Raphael <ll.doveland@gmail.com>
2018-06-10 22:19:32 -0700

    Updated  writeWaveHeader class, and draft for ASR implementation

--- src/org/vitrivr/cineast/core/features/exporter/ASRImplementation.java
+++ src/org/vitrivr/cineast/core/features/exporter/ASRImplementation.java
@@ -3,7 +3,9 @@ package org.vitrivr.cineast.core.features.exporter;
 import static java.nio.file.StandardOpenOption.CREATE;
 import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
@@ -23,15 +25,19 @@ import org.vitrivr.cineast.core.db.PersistencyWriterSupplier;
 import org.vitrivr.cineast.core.features.extractor.Extractor;
 import org.vitrivr.cineast.core.setup.EntityCreator;
 import org.vitrivr.cineast.core.util.LogHelper;
+import org.vitrivr.cineast.explorative.CommandLine;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 
 /**
- * Exports the audio in a given segment as mono WAV file.
+ * Exports deepspeech translation
  *
- * @author rgasser
+ * @author huirong
  * @version 1.0
- * @created 31.01.17
+ * @created 06.06.2018
  */
-public class AudioSegmentExporter implements Extractor {
+
+public class ASRImplementation implements Extractor {
 
 
     private static final Logger LOGGER = LogManager.getLogger();
@@ -40,6 +46,8 @@ public class AudioSegmentExporter implements Extractor {
 
     /** Destination path for the audio-segment. */
     private Path destination;
+
+    private String pathToDeepSpeechModel = "/Users/TestProgram";
 
     /**
      * Default constructor. The AudioSegmentExport can be configured via named properties
@@ -51,7 +59,7 @@ public class AudioSegmentExporter implements Extractor {
      *
      * @param properties HashMap containing named properties
      */
-    public AudioSegmentExporter(HashMap<String, String> properties) {
+    public ASRImplementation(HashMap<String, String> properties) {
         if (properties.containsKey(PROPERTY_NAME_DESTINATION)) {
             this.destination = Paths.get(properties.get(PROPERTY_NAME_DESTINATION));
         } else {
@@ -64,6 +72,7 @@ public class AudioSegmentExporter implements Extractor {
      *
      * @param shot SegmentContainer to process.
      */
+
     @Override
     public void processSegment(SegmentContainer shot) {
         try {
@@ -72,13 +81,27 @@ public class AudioSegmentExporter implements Extractor {
             Files.createDirectories(directory);
             OutputStream stream = Files.newOutputStream(directory.resolve(shot.getId()+".wav"), CREATE, TRUNCATE_EXISTING);
 
+            String fileName = shot.getId()+".wav";
+
+            //in mac oxs
+            String command = "deepspeech " + pathToDeepSpeechModel + "output_graph.pb " +
+                    pathToDeepSpeechModel + "alphabet.txt " +
+                    pathToDeepSpeechModel + "lm.binary " +
+                    pathToDeepSpeechModel + "trie " +
+                    fileName ;
+
+
+            String output = executeCommand(command);
+            System.out.println(command + "\n");
+            System.out.println(output);
+
+
             /* Extract mean samples and perpare byte buffer. */
             short[] data = shot.getMeanSamplesAsShort();
             ByteBuffer buffer = ByteBuffer.allocate(44 + data.length*2).order(ByteOrder.LITTLE_ENDIAN);
 
             /* Write header of WAV file. */
-            short channels = 1;
-            this.writeWaveHeader(buffer, shot.getSamplingrate(), channels,data.length * 2);
+            this.writeWaveHeader(buffer, 16000, data.length * 2);
 
             /* Write actual data. */
             for (short sample : data) {
@@ -87,6 +110,8 @@ public class AudioSegmentExporter implements Extractor {
 
             stream.write(buffer.array());
             stream.close();
+
+
         } catch (IOException | BufferOverflowException e) {
             LOGGER.fatal("Could not export audio segment {} due to a serious IO error ({}).", shot.getId(), LogHelper.getStackTrace(e));
         }
@@ -99,16 +124,19 @@ public class AudioSegmentExporter implements Extractor {
      * @param samplingrate Samplingrate of the output file.
      * @param length Length in bytes of the frames data
      */
-    private void writeWaveHeader(ByteBuffer buffer, float samplingrate, short channels, int length) {
-    /* Length of the subChunk2. */
+
+    private void writeWaveHeader(ByteBuffer buffer, float samplingrate,  int length) {
+        short channels = 1;
+
+        /* Length of the subChunk2. */
         final int subChunk2Length = length * channels * (AudioFrame.BITS_PER_SAMPLE/8); /* Number of bytes for audio data: NumSamples * NumChannels * BitsPerSample/8. */
 
-    /* RIFF Chunk. */
+        /* RIFF Chunk. */
         buffer.put("RIFF".getBytes());
         buffer.putInt(36 + subChunk2Length);
         buffer.put("WAVE".getBytes()); /* WAV format. */
 
-    /* Format chunk. */
+        /* Format chunk. */
         buffer.put("fmt ".getBytes()); /* Begin of the format chunk. */
         buffer.putInt(16); /* Length of the Format chunk. */
         buffer.putShort((short)1); /* Format: 1 = Raw PCM (linear quantization). */
@@ -118,9 +146,33 @@ public class AudioSegmentExporter implements Extractor {
         buffer.putShort((short)(channels * (AudioFrame.BITS_PER_SAMPLE/8))); /* Block align: NumChannels * BitsPerSample/8. */
         buffer.putShort((short)(AudioFrame.BITS_PER_SAMPLE)) /* Bits per sample. */;
 
-    /* Data chunk */
+        /* Data chunk */
         buffer.put("data".getBytes()); /* Begin of the data chunk. */
         buffer.putInt(subChunk2Length); /* Length of the data chunk. */
+    }
+
+    private String executeCommand(String command) {
+
+        StringBuffer output = new StringBuffer();
+
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine())!= null) {
+                output.append(line + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+
     }
 
 
